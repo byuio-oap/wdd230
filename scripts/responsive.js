bc95029d5dcf0c8be3f7d0bf53c44e1ac08052a4
Orlando Ancelmo Pinelo <orlandoancelmopinelo@gmail.com>
2024-01-27 16:12:10 -0600

    Xela Site Plan

--- scripts/responsive.js
+++ scripts/responsive.js
@@ -1,10 +1,3 @@
-// const HamburgerMenuButton=document.querySelector("#menu");
-// const NavOptions=document.querySelector("nav");
-
-// HamburgerMenuButton.addEventListener("click",()=>{
-//     NavOptions.classList.toggle("open");
-//     HamburgerMenuButton.classList.toggle("open");
-// })
 
 const hamButton = document.querySelector('#menu');
 const navigation = document.querySelector('.navigation');
