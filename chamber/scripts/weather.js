3037db7c87e696201a6b57ed38312b4336b2334f
Orlando Ancelmo Pinelo <orlandoancelmopinelo@gmail.com>
2024-03-18 07:55:47 -0600

    weather updates

---
+++ chamber/scripts/weather.js
@@ -0,0 +1,130 @@
+
+
+
+// Get Current Weather
+const urlWeather = 'https://api.openweathermap.org/data/2.5/weather?lat=14.85&lon=-91.53&units=imperial&appid=d48e24b6d05f6a9d5a7f668a71f9609f';
+const urlForecast ='https://api.openweathermap.org/data/2.5/forecast?lat=14.85&lon=-91.53&units=imperial&appid=d48e24b6d05f6a9d5a7f668a71f9609f';
+
+
+
+
+
+//Show and hide banner
+
+document.addEventListener('DOMContentLoaded', function() {
+      var fecha=new Date();
+      var todayDayNumber=fecha.getDay();
+      if (todayDayNumber>=1 && todayDayNumber<=3) {
+        document.getElementById("chamberBanner").style.display="block";
+        
+      }
+
+});
+
+document.getElementById("closeButton").addEventListener("click", function() {
+  document.getElementById("chamberBanner").style.display = "none";
+});
+  
+
+// Fetch weather From appi
+  async function apiFetchWeather() {
+    try {
+        const response = await fetch(urlWeather);
+        if (response.ok) {
+            const data = await response.json();
+            displayResults(data);
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+apiFetchWeather();
+
+//Display todays weather
+function displayResults(data) {
+
+  document.getElementById("desc").textContent = data.weather[0].description;
+  document.getElementById("temp").textContent = `${data.main.temp.toFixed(0) + "\xB0F"}`;
+  
+
+}
+
+//fetch Foreast Api  
+async function apiFetchForecast() {
+  try {
+      const response = await fetch(urlForecast);
+      if (response.ok) {
+          const data = await response.json();
+          displayForecastResults(data);
+      } else {
+          throw Error(await response.text());
+      }
+  } catch (error) {
+      console.log(error);
+  }
+}
+apiFetchForecast();
+
+//Display 3 day forecast starting tommorow
+function displayForecastResults(data){
+  const d=new Date();
+  const todayDayNumber=d.getDay();
+  console.log(todayDayNumber);
+  const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    const weatherList=data.list;
+    let forecastDayNumber=todayDayNumber;
+    let days = 0;
+    //iterate 
+    weatherList.forEach((weather) => {
+      let time=weather.dt_txt;
+      console.log(time);
+
+      if (time.includes("18:00:00") && days <3) {
+        forecastDayNumber+=1;
+        if (forecastDayNumber ===7) {
+            forecastDayNumber=0;
+        }
+          const forecastItem =document.createElement("div");
+          forecastItem.classList="forecast-item";
+          const dayName=document.createElement("h4");
+
+          dayName.textContent=weekday[forecastDayNumber]
+          
+          const iconsrc = "//openweathermap.org/img/wn/" + weather.weather[0].icon + "@2x.png"
+          const weatherIcon=document.createElement("img");
+          
+          let desc = weather.weather[0].description;
+          let captionDesc = document.createElement("figcaption");
+          let capDesc=desc;
+          weatherIcon.setAttribute('src', iconsrc);
+          weatherIcon.setAttribute('alt', capDesc);
+     
+          captionDesc.textContent=capDesc;
+          const temp = document.createElement("p");
+
+          temp.textContent = weather.main.temp.toFixed(0) +"\xB0F";
+
+
+
+          forecastItem.appendChild(dayName);
+          forecastItem.appendChild(weatherIcon);
+          forecastItem.appendChild(captionDesc)
+          forecastItem.appendChild(temp);
+
+          document.querySelector(".forecast-box").appendChild(forecastItem);
+
+          days++;
+
+        
+      }
+
+    }
+
+
+)};
+
+
+
